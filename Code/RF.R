0db1575239e2c146570c630e777ca267de8207ce
jbhender <zhaoleo@umich.edu>
2021-12-17 16:24:18 -0500

    update the code and comments in RF

--- Code/RF.R
+++ Code/RF.R
@@ -1,22 +1,25 @@
 library(randomForest)
-
+library(tictoc)
+library(caret)
+library(performanceEstimation)
 year = seq(1999,2017,2)
 namelist = paste(rep("data",length(year)),year,rep("_",length(year)),year+1,sep = "")
 for(i in namelist){
-  temp =  readRDS(paste("Data/",i,".rds",sep = ""))
+  temp =  readRDS(paste("Results/Data/",i,".rds",sep = ""))
   eval(parse(text = paste0(i,"<- temp")))
 }
-# Xgboost function returning the model, chosen parameters and confusion matrix
+# RF function returning the model, chosen parameters and confusion matrix
 rf = function(train.data, test.data){
   Variable.Importance =randomForest(DIQ010~., data = train.data,ntree = 2000,
                                     mtry = 19,importance = TRUE)
   rf.pre = predict(Variable.Importance, test.data,type="class")
   #confusion matrix
-  cm.f <- confusionMatrix(rf.pre,test.data$DIQ010,positive='1');cm.f
+  cm.f <- confusionMatrix(rf.pre,test.data$DIQ010,positive='1');
   result = cm.f$byClass
-  return(list(model = fit.xg, result = result))
+  return(list(model =  Variable.Importance, result = result))
 }
 tbl1 = c()
+time_out = rep(0,length(namelist))
 for(i in 1:length(namelist)){
   tic()
   tmp = get(namelist[i])
@@ -29,8 +32,10 @@ for(i in 1:length(namelist)){
   # rf function
   result = rf(train.data, test.data)
   time_return = toc()
-  save(result, file=paste0("Results/Selection Results/Xgboost_result/Xgboost_",namelist[i], ".RData"))
+  save(result, file=paste0("Results/Selection Results/RF/RF_",namelist[i], ".RData"))
   time_out[i] = time_return$toc - time_return$tic
   print(time_out[i])
   print(paste0("finish for data", namelist[i]))
 }
+
+save(time_out,file = "Results/Selection Results/RF/timeout.RData")
